3ac910bc1a4deed13a3ddde813464004c641a902
reyadussalahin <reyadussalahin@gmail.com>
2020-09-29 00:05:26 +0600

    fixed composer.json for removing .git after create-project cmd

---
+++ setup.php
@@ -0,0 +1,22 @@
+<?php
+
+// defining utility functions
+function removeDirectoryRecursively($dir) {
+    if(is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach($objects as $object) { 
+            if ($object != "." && $object != "..") { 
+                if(is_dir($dir . DIRECTORY_SEPARATOR . $object) && !is_link($dir . "/" . $object)) {
+                    removeDirectoryRecursively($dir . DIRECTORY_SEPARATOR . $object);
+                } else {
+                    unlink($dir . DIRECTORY_SEPARATOR . $object);
+                }
+            }
+        }
+        rmdir($dir); 
+    }
+}
+
+
+// removing .git
+removeDirectoryRecursively(".git");
