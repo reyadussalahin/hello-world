efe3b83ddcacd64ba8ee1df780bcf517ccdad807
reyadussalahin <reyadussalahin@gmail.com>
2020-10-12 23:47:52 +0600

    added example for api routes

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,54 @@
 <?php
 
+/**
+ * Let's assume there's a class 'UserController'
+ * in your {project_base_dir}/App/Api/Controllers directory
+ * then
+ */
+
+
+use App\Api\Controllers\UserController;
+
+
 return [
+    "GET" => [
+        "/users" => [
+            "name" => "users",
+            "target" => [UserController::class, "index"]
+        ],
+        "/user/{id}" => [
+            "name" => "user.show",
+            "target" => [UserController::class, "show"],
+            "filter" => [
+                "id" => "[0-9]+"
+            ]
+        ]
+    ],
+
+    "POST" => [
+        "/user" => [
+            "name" => "user.store",
+            "target" => [UserController::class, "store"]
+        ]
+    ],
+
+    "PATCH" => [
+        "/user/{id}" => [
+            "name" => "user.update",
+            "target" => [UserController::class, "update"],
+            "filter" => [
+                "id" => "[0-9]+"
+            ]
+        ]
+    ],
+
+    "DELETE" => [
+        "/user/{id}" => [
+            "name" => "user.delete",
+            "target" => [UserController::class, "delete"],
+            "filter" => [
+                "id" => "[0-9]+"
+            ]
+        ]
+    ]
 ];
